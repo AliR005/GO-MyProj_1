35f7f656adb964bf7bb2c46d773db9f96584820a
Ali Radjabov <alir@MacBook-Air-Ali.local>
2024-08-31 17:43:54 +0300

    part echo bot

--- main.go
+++ main.go
@@ -1,22 +1,43 @@
 package main
 
 import (
-	"flag"
+	"fmt"
 	"log"
+	"os"
+
+	"github.com/mymmrac/telego"
+	tu "github.com/mymmrac/telego/telegoutil"
 )
 
 func main(){
+	TOKEN := os.Getenv("TOKEN_BOT")
+	bot := create_bot(TOKEN)
 
+	updates , _ := bot.UpdatesViaLongPolling(nil)
+	defer bot.StopLongPolling()
+
+	for update := range updates {
+		if update.Message != nil {
+			chatID := update.Message.Chat.ID
+
+			sentMessage, _ := bot.SendMessage(
+				tu.Message(
+					tu.ID(chatID),
+					update.Message.Text,
+				),
+			)
+
+			fmt.Printf("Sent Message: %v\n", sentMessage)
+		}
+	}
 
 }
 
-func mustToken () string {
-	token := flag.String("token_bot", "", "token for access tg bot")
-	flag.Parse()
-
-	if *token == "" {
+func create_bot(token string) *telego.Bot{
+	bot, err := telego.NewBot(token, telego.WithDefaultDebugLogger())
+	if err != nil{
+		fmt.Println(err)
 		log.Fatal("token is not specified")
 	}
-	
-	return *token
+	return bot
 }
